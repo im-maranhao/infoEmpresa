9e103b22a892feaff22a55e549f0cffcfe4b9455
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2025-12-11 23:51:30 -0300

    Atualizar o Main.java

--- src/br/com/miriam/Main.java
+++ src/br/com/miriam/Main.java
@@ -9,7 +9,6 @@ public class Main {
         Funcionario funcionario = new Funcionario("Lídia Maranhão", "123.456.789-10", "Farmaceutica", 3500.00);
         Gerente gerente = new Gerente("Joeli Maranhão", "252.***.***-09", "Gerente", 4000.00, 1500.00);
         Desenvolvedor desenvolvedor = new Desenvolvedor("Miriam Maranhão da Silva", "454.***.***-95", "Dev Backend Java Junior", 3500.00, 20, 50.00);
-
         System.out.println("\n------Informações do Funcionario------");
         funcionario.exibirInformacoes();
 
